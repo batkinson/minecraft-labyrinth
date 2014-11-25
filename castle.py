be1bf144fae1b8c13c26cf69e726b3674a6d730a
Brent Atkinson <brent.atkinson@gmail.com>
2014-11-25 14:35:05 -0500

    Working generation of castle.

--- castle.py
+++ castle.py
@@ -1,135 +1,154 @@
 #!/usr/bin/python
 
+from dimensions import *
 import mcpi.minecraft as minecraft
 import mcpi.block as block
 from threading import Thread
+from time import sleep
 
 
 def get_conn():
- return minecraft.Minecraft.create(address='checkers.local')
+    return minecraft.Minecraft.create(address='checkers.local')
 
-#--------------------------------------
-# Define Functions
-#--------------------------------------
+mc = get_conn()
 
-def CreateWalls(size,baseheight,height,material,battlements,walkway):
 
-  mc = get_conn()
+def create_walls(size, baseheight, height, material=block.STONE_BRICK, battlements=True, walkway=True):
 
-  # Create 4 walls with a specified width, height and material.
-  # Battlements and walkways can also be added to the top edges.
-  
-  mc.setBlocks(-size,baseheight+1,-size,size,baseheight+height,-size,material) 
-  mc.setBlocks(-size,baseheight+1,-size,-size,baseheight+height,size,material)
-  mc.setBlocks(size,baseheight+1,size,-size,baseheight+height,size,material) 
-  mc.setBlocks(size,baseheight+1,size,size,baseheight+height,-size,material) 
+    """Creates four walls."""
 
-  # Add battlements to top edge
-  if battlements==True:
-    for x in range(0,(2*size)+1,2):
-      mc.setBlock(size,baseheight+height+1,(x-size),material) 
-      mc.setBlock(-size,baseheight+height+1,(x-size),material) 
-      mc.setBlock((x-size),baseheight+height+1,size,material) 
-      mc.setBlock((x-size),baseheight+height+1,-size,material)
-      
-  # Add wooden walkways
-  if walkway==True:  
-    mc.setBlocks(-size+1,baseheight+height-1,size-1,size-1,baseheight+height-1,size-1,block.WOOD_PLANKS)   
-    mc.setBlocks(-size+1,baseheight+height-1,-size+1,size-1,baseheight+height-1,-size+1,block.WOOD_PLANKS)  
-    mc.setBlocks(-size+1,baseheight+height-1,-size+1,-size+1,baseheight+height-1,size-1,block.WOOD_PLANKS)   
-    mc.setBlocks(size-1,baseheight+height-1,-size+1,size-1,baseheight+height-1,size-1,block.WOOD_PLANKS)  
+    mc.setBlocks(-size, baseheight + 1, -size, size, baseheight + height, -size, material)
+    mc.setBlocks(-size, baseheight + 1, -size, -size, baseheight + height, size, material)
+    mc.setBlocks(size, baseheight + 1, size, -size, baseheight + height, size, material)
+    mc.setBlocks(size, baseheight + 1, size, size, baseheight + height, -size, material)
 
-def CreateLandscape(moatwidth,moatdepth,islandwidth):
-  
-  mc = get_conn()
+    # Add battlements to top edge
+    if battlements:
+        for x in range(0, (2 * size) + 1, 2):
+            mc.setBlock(size, baseheight + height + 1, (x - size), material)
+            mc.setBlock(-size, baseheight + height + 1, (x - size), material)
+            mc.setBlock((x - size), baseheight + height + 1, size, material)
+            mc.setBlock((x - size), baseheight + height + 1, -size, material)
 
-  # Set upper half to air
-  mc.setBlocks(-128,1,-128,128,128,128,block.AIR) 
-  # Set lower half of world to dirt with a layer of grass
-  mc.setBlocks(-128,-1,-128,128,-128,128,block.DIRT)
-  mc.setBlocks(-128,0,-128,128,0,128,block.GRASS)
-  # Create water moat
-  mc.setBlocks(-moatwidth,0,-moatwidth,moatwidth,-moatdepth,moatwidth,block.WATER)
-  # Create island inside moat
-  mc.setBlocks(-islandwidth,0,-islandwidth,islandwidth,1,islandwidth,block.GRASS)  
+    # Add wooden walkways
+    if walkway:
+        mc.setBlocks(-size + 1, baseheight + height - 1, size - 1, size - 1, baseheight + height - 1, size - 1,
+                     block.WOOD_PLANKS)
+        mc.setBlocks(-size + 1, baseheight + height - 1, -size + 1, size - 1, baseheight + height - 1, -size + 1,
+                     block.WOOD_PLANKS)
+        mc.setBlocks(-size + 1, baseheight + height - 1, -size + 1, -size + 1, baseheight + height - 1, size - 1,
+                     block.WOOD_PLANKS)
+        mc.setBlocks(size - 1, baseheight + height - 1, -size + 1, size - 1, baseheight + height - 1, size - 1,
+                     block.WOOD_PLANKS)
+        # Add ladder to access walkway
+        mc.setBlocks(-size + 1, baseheight + 1, 0, -size + 1, baseheight + height, 0, block.LADDER.withData(5))
 
-def CreateKeep(size,baseheight,levels):
-  
-  mc = get_conn()
 
-  # Create a keep with a specified number
-  # of floors levels and a roof
-  height=(levels*5)+5
-  
-  CreateWalls(size,baseheight,height,block.STONE_BRICK,True,True)
-  
-  # Floors & Windows
-  for level in range(1,levels+1):
-    mc.setBlocks(-size+1,(level*5)+baseheight,-size+1,size-1,(level*5)+baseheight,size-1,block.WOOD_PLANKS)
+def create_grounds(moatwidth, moatdepth, islandwidth):
 
-  # Windows
-  for level in range(1,levels+1):
-    CreateWindows(0,(level*5)+baseheight+2,size,"N")
-    CreateWindows(0,(level*5)+baseheight+2,-size,"S")
-    CreateWindows(-size,(level*5)+baseheight+2,0,"W")
-    CreateWindows(size,(level*5)+baseheight+2,0,"E")
+    # Set upper half to air
+    mc.setBlocks(x_min, 1, z_min, x_max, y_max, z_max, block.AIR)
+    # Set lower half of world to dirt with a layer of grass
+    mc.setBlocks(x_min, -1, z_min, x_max, y_min, z_max, block.DIRT)
+    mc.setBlocks(x_min, 0, z_min, x_max, 0, z_max, block.GRASS)
+    # Create water moat
+    mc.setBlocks(-moatwidth, 0, -moatwidth, moatwidth, -moatdepth, moatwidth, block.WATER)
+    # Replace the ground under the castle
+    mc.setBlocks(-islandwidth, -1, -islandwidth, islandwidth, -moatdepth, islandwidth, block.DIRT)
+    mc.setBlocks(-islandwidth, 0, -islandwidth, islandwidth, 0, islandwidth, block.GRASS)
+    # Place planking through castle and over moat
+    mc.setBlocks(5, 0, -1, moatwidth, 0, 1, block.WOOD_PLANKS)
 
-  # Door
-  mc.setBlocks(0,baseheight+1,size,0,baseheight+2,size,block.AIR)
 
-def CreateWindows(x,y,z,dir):
-  
-  mc = get_conn()
+def create_keep(size=5, baseheight=0, levels=4):
 
-  if dir=="N" or dir=="S":
-    z1=z
-    z2=z
-    x1=x-2
-    x2=x+2
+    height = (levels * 5) + 5
 
-  if dir=="E" or dir=="W":
-    z1=z-2
-    z2=z+2
-    x1=x
-    x2=x
+    create_walls(size, baseheight, height, block.STONE_BRICK, True, True)
 
-  mc.setBlocks(x1,y,z1,x1,y+1,z1,block.AIR)
-  mc.setBlocks(x2,y,z2,x2,y+1,z2,block.AIR) 
+    # Floors & Windows
+    for level in range(1, levels + 1):
+        mc.setBlocks(-size + 1, (level * 5) + baseheight, -size + 1, size - 1, (level * 5) + baseheight, size - 1,
+                     block.WOOD_PLANKS)
 
-  if dir=="N":
-    a=3
-  if dir=="S":
-    a=2
-  if dir=="W":
-    a=0
-  if dir=="E":
-    a=1
+    # Windows
+    for level in range(1, levels + 1):
+        create_windows(0, (level * 5) + baseheight + 2, size, "N")
+        create_windows(0, (level * 5) + baseheight + 2, -size, "S")
+        create_windows(-size, (level * 5) + baseheight + 2, 0, "W")
+        create_windows(size, (level * 5) + baseheight + 2, 0, "E")
 
-  mc.setBlock(x1,y-1,z1,109,a)
-  mc.setBlock(x2,y-1,z2,109,a)
-  
-#--------------------------------------
-#
-# Main Script  
-#
-#--------------------------------------
+    # Door
+    mc.setBlocks(0, baseheight + 1, size, 0, baseheight + 2, size, block.AIR)
+    mc.setBlock(0, baseheight + 1, size, block.DOOR_WOOD.withData(3))
+    mc.setBlock(0, baseheight + 2, size, block.DOOR_WOOD.withData(8))
 
-def buildCastle():
-  
-   mc = get_conn()
+    # Replace ladder
+    mc.setBlocks(-4, 1, 0, -4, 25, 0, block.AIR)
+    mc.setBlocks(-4, 1, 0, -4, 25, 0, block.LADDER.withData(5))
 
-   """Builds a castle concurrently."""
-   mc.postToChat('Castle time!!!')
-   ls = Thread(target=CreateLandscape, args=[33, 10, 23])
-   cw1 = Thread(target=CreateWalls, args=[21, 1, 5, block.STONE_BRICK, True, True])
-   cw2 = Thread(target=CreateWalls, args=[13, 1, 6, block.STONE_BRICK, True, True])
-   kp = Thread(target=CreateKeep, args=[5, 1, 4])
-   ls.start()
-   cw1.start()
-   cw2.start()
-   kp.start()
-   for t in [ls, cw1, cw2, kp]:
-      t.join()
-   mc.postToChat('Castle time over!')
-   mc.player.setPos(0,30,4)
+
+def create_windows(x, y, z, direction):
+
+    if direction == "N" or direction == "S":
+        z1 = z
+        z2 = z
+        x1 = x - 2
+        x2 = x + 2
+
+    if direction == "E" or direction == "W":
+        z1 = z - 2
+        z2 = z + 2
+        x1 = x
+        x2 = x
+
+    mc.setBlocks(x1, y, z1, x1, y + 1, z1, block.AIR)
+    mc.setBlocks(x2, y, z2, x2, y + 1, z2, block.AIR)
+
+    if direction == "N":
+        a = 3
+    if direction == "S":
+        a = 2
+    if direction == "W":
+        a = 0
+    if direction == "E":
+        a = 1
+
+    mc.setBlock(x1, y - 1, z1, 109, a)
+    mc.setBlock(x2, y - 1, z2, 109, a)
+
+
+def build_kingdom():
+
+    mc.postToChat('Building kingdom, this may take a while...')
+    sleep(.5)
+
+    def build_it():
+        create_grounds(33, 10, 23)
+        create_walls(21, 0, 5)
+        create_walls(13, 0, 6)
+        create_keep()
+        # Plant flowers in front
+        mc.setBlocks(23, 1, -23, 22, 1, 23, block.FLOWER_YELLOW)
+        # Build castle streets
+        mc.setBlocks(-21, 0, -21, 21, 0, 21, block.MOSS_STONE)
+        mc.setBlocks(-18, 0, -18, 18, 0, 18, block.COBBLESTONE)
+        mc.setBlocks(-15, 0, -15, 15, 0, 15, block.MOSS_STONE)
+        mc.setBlocks(-10, 0, -10, 10, 0, 10, block.COBBLESTONE)
+        mc.setBlocks(-7, 0, -7, 7, 0, 7, block.MOSS_STONE)
+        mc.setBlocks(-5, 0, -5, 5, 0, 5, block.COBBLESTONE)
+        mc.setBlocks(8, 0, -1, 21, 0, 1, block.COBBLESTONE)
+        mc.setBlocks(0, 0, 6, 0, 0, 7, block.COBBLESTONE)
+        # Creates gateways
+        mc.setBlocks(13, 1, -1, 23, 3, 1, block.AIR)
+        # Position player
+        mc.player.setPos(0, 30, 4)
+        mc.postToChat('Construction complete!')
+
+    build_thread = Thread(target=build_it)
+    build_thread.start()
+    build_thread.join()
+
+
+
 
---
+++ dimensions.py
@@ -0,0 +1,85 @@
+from mcpi import minecraft, block
+
+m = minecraft.Minecraft.create(address='checkers.local')
+
+MAX_DIM = 256
+BOUNDARY = block.BEDROCK_INVISIBLE.id
+
+__all__ = ['x_min', 'x_max', 'z_min', 'z_max', 'y_min', 'y_max', 'x_center', 'z_center']
+
+def is_boundary(x, y, z):
+    """True if the block at x,y,z is invisible bedrock."""
+    return m.getBlock(x, y, z) == BOUNDARY
+
+
+def find_x_max(start=0, end=MAX_DIM):
+    """Find highest x value with usable blocks."""
+    mid = (start + end) / 2
+    if is_boundary(mid, 0, 0):
+        return find_x_max(start, mid - 1)
+    elif is_boundary(mid + 1, 0, 0):
+        return mid
+    else:
+        return find_x_max(mid + 1, end)
+
+
+def find_x_min(start=0, end=-MAX_DIM):
+    """Find lowest x value with usable blocks."""
+    mid = (start + end) / 2
+    if is_boundary(mid, 0, 0):
+        return find_x_min(start, mid + 1)
+    elif is_boundary(mid - 1, 0, 0):
+        return mid
+    else:
+        return find_x_min(mid - 1, end)
+
+
+def find_z_max(start=0, end=MAX_DIM):
+    """Find highest z value with usable blocks."""
+    mid = (start + end) / 2
+    if is_boundary(0, 0, mid):
+        return find_z_max(start, mid - 1)
+    elif is_boundary(0, 0, mid + 1):
+        return mid
+    else:
+        return find_z_max(mid + 1, end)
+
+
+def find_z_min(start=0, end=-MAX_DIM):
+    """Find lowest z value with usable blocks."""
+    mid = (start + end) / 2
+    if is_boundary(0, 0, mid):
+        return find_z_min(start, mid + 1)
+    elif is_boundary(0, 0, mid - 1):
+        return mid
+    else:
+        return find_z_min(mid - 1, end)
+
+
+def x_dim():
+    """Returns the usable x dimensions as a tuple, (min, max)."""
+    return (find_x_min(), find_x_max())
+
+
+def z_dim():
+    """Returns the usable z dimensions as a tuple, (min, max)."""
+    return (find_z_min(), find_z_max())
+
+
+x_min, x_max = x_dim()
+z_min, z_max = z_dim()
+
+# y dimension doesn't matter as much, for now
+y_min, y_max = -128, 128
+
+x_center, z_center = (x_min + x_max) / 2, (z_min + z_max) / 2
+
+
+def mark_boundary(material=block.GLOWING_OBSIDIAN):
+    """Marks the x/z boundary of the world."""
+    for x in xrange(x_min + 1, x_max):
+        m.setBlock(x, m.getHeight(x, z_max) - 1, z_max, material)
+        m.setBlock(x, m.getHeight(x, z_min) - 1, z_min, material)
+    for z in xrange(z_min, z_max + 1):
+        m.setBlock(x_max, m.getHeight(x_max, z) - 1, z, material)
+        m.setBlock(x_min, m.getHeight(x_min, z) - 1, z, material)
